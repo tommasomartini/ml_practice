35463e333bd9ca34df8f8608f8ce3293ce5da823
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2019-11-09 23:07:49 +0100

    Completed assignment 2

--- bayesian_learning/assignment.py
+++ bayesian_learning/assignment.py
@@ -8,6 +8,18 @@ import bayesian_learning.plotting as plotting
 sns.set()
 
 
+def _separator():
+    sep_line = '#' * 80
+    print()
+    print(sep_line)
+    print()
+
+
+def _evaluate_accuracy(predictions, labels):
+    accuracy = np.mean(predictions == labels)
+    return accuracy
+
+
 def _covariance(samples):
     """Computes the covariance matrix of the input samples.
 
@@ -80,6 +92,90 @@ def maximum_likelihood_estimator(samples, labels, weights=None, naive=True):
     return mu, sigma
 
 
+def compute_priors(labels, weights=None):
+    """Computes the priors on the labels.
+
+    In this simple implementation the prior is computed as the frequency
+    each label appears.
+
+    Args:
+        labels: Shape=(N,); label for each sample.
+        weights: TODO
+
+    Returns:
+        A vector of probabilities with shape=(C,) with C number of classes.
+    """
+    N = len(labels)
+    if weights is None:
+        weights = np.ones(N) / N
+    else:
+        assert (len(weights) == N)
+
+    classes = sorted(list(set(labels)))
+    priors = np.array([len(np.where(labels == class_id)[0])
+                       for class_id in classes]) / N
+    return priors
+
+
+# in:      X - N x d matrix of M data points
+#      prior - C x 1 matrix of class priors
+#         mu - C x d matrix of class means (mu[i] - class i mean)
+#      sigma - C x d x d matrix of class covariances (sigma[i] - class i sigma)
+# out:     h - N vector of class predictions for test points
+def classify_bayes(samples, priors, mu, sigma):
+    """Uses the Bayes Theorem to classify the provided samples.
+
+    Args:
+        samples: Shape=(N, D); samples to classify, where N is the number of
+            samples and D their dimensionality.
+        priors: Shape=(C,); priors on the labels, with C number of classes.
+        mu: Shape=(C, D); estimated means, for each class.
+        sigma: Shape=(C, D, D); estimated covariance matrices, for each class.
+
+    Returns:
+        A vector with shape=(N,) with the class prediction for each sample.
+    """
+    N = samples.shape[0]
+    C, D = mu.shape
+
+    def _log_prob(samples_, mu_, sigma_, prior_):
+        """Computes the logarithm of the Gaussian probability of
+        the provided samples.
+
+        Args:
+            samples_: Shape=(N, D).
+            mu_: Shape=(D,).
+            sigma_: Shape=(D, D).
+            prior_: Scalar.
+
+        Returns:
+            A vector with shape=(N,) with the log-probabilities of the N
+            input samples..
+        """
+        aux1 = -0.5 * np.log(np.linalg.det(sigma_))      # scalar
+        aux2 = samples_ - mu_[np.newaxis, :]            # (N, D)
+        sigma_inv = np.linalg.inv(sigma_)                # (D, D)
+        aux3 = aux2 @ sigma_inv                         # (N, D)
+        aux4 = aux3 @ aux2.T                            # (N, N)
+        aux5 = -0.5 * np.diag(aux4)                     # (N,)
+        log_prob_ = aux1 + aux5 + np.log(prior_)
+        return log_prob_
+
+    # Generate a (N, C) matrix.
+    log_probs = np.array([
+        _log_prob(samples_=samples,
+                  mu_=mu[class_idx],
+                  sigma_=sigma[class_idx],
+                  prior_=priors[class_idx])
+        for class_idx in range(C)
+    ]).T
+
+    assert log_probs.shape == (N, C)
+
+    predictions = np.argmax(log_probs, axis=1)
+    return predictions
+
+
 def assignment1():
     print('Assignment 1')
     print('Compute the Maximum Likelihood estimation '
@@ -105,8 +201,30 @@ def assignment1():
     plt.close()
 
 
+def assignment2():
+    print('Assignment 2')
+    print('Compute the prior and classify using the Bayesian rule.')
+
+    # Generate a dataset.
+    samples, labels = make_blobs(n_samples=200,
+                                 centers=5,
+                                 n_features=2,
+                                 random_state=0)
+    mu, sigma = maximum_likelihood_estimator(samples, labels, naive=False)
+    priors = compute_priors(labels)
+    predictions = classify_bayes(samples=samples,
+                                 priors=priors,
+                                 mu=mu,
+                                 sigma=sigma)
+
+    accuracy = _evaluate_accuracy(predictions, labels)
+    print('Accuracy: {:.3f}'.format(accuracy))
+
+
 def main():
-    assignment1()
+    # assignment1()
+    # _separator()
+    assignment2()
 
 
 if __name__ == '__main__':
