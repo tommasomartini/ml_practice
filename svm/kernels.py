dd9b6a5a909ebb9b4740757aa76725e78ff1752f
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2019-11-09 15:55:17 +0100

    Fix bug in RBF kernel dimensions

--- svm/kernels.py
+++ svm/kernels.py
@@ -3,10 +3,12 @@ Each function returns a kernel function, which has the following
 signature:
     res = _ker(v1, v2)
 with:
-    v1, v2: Array-like with shape=(N, D) or (D,) where N is the number of points
+    v1: Array-like with shape=(N1, D) or (D,) where N1 is the number of points
+        and D is the dimensionality of each point.
+    v2: Array-like with shape=(N2, D) or (D,) where N2 is the number of points
         and D is the dimensionality of each point.
 
-    res: Scalar or array-like with shape=(N,).
+    res: Scalar or array-like with shape=(N1, N2).
 """
 
 import numpy as np
@@ -42,10 +44,19 @@ def get_radial_basis_function_kernel(sigma):
     """
     def _ker(v1, v2):
         v1, v2 = np.atleast_2d(v1, v2)
-        aux1 = np.linalg.norm(v1 - v2, axis=1) ** 2
-        aux2 = - aux1 / sigma ** 2
-        aux3 = np.exp(aux2)
-        res = np.squeeze(aux3)
+
+        N1, D1 = v1.shape
+        N2, D2 = v2.shape
+        assert D1 == D2
+        D = D1
+
+        v1_r = v1.reshape((N1, 1, D))
+        v2_r = v2.reshape((1, N2, D))
+
+        aux1 = v1_r - v2_r                          # (N1, N2, D)
+        aux2 = np.linalg.norm(aux1, axis=-1) ** 2   # (N1, N2)
+        aux3 = - aux2 / sigma ** 2                  # (N1, N2)
+        res = np.squeeze(np.exp(aux3))              # (N1, N2)
         return res
 
     return _ker
