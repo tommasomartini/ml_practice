f96017e462ca3301193ad935a0e7ee75ef5cc3a3
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2019-11-09 12:26:38 +0100

    Add minimal docstring to kernels

--- svm/kernels.py
+++ svm/kernels.py
@@ -1,7 +1,21 @@
+"""This module contains a collection of kernel function generators.
+Each function returns a kernel function, which has the following
+signature:
+    res = _ker(v1, v2)
+with:
+    v1, v2: Array-like with shape=(N, D) or (D,) where N is the number of points
+        and D is the dimensionality of each point.
+
+    res: Scalar or array-like with shape=(N,).
+"""
+
 import numpy as np
 
 
 def get_linear_kernel():
+    """Implements:
+        <x.T, y> + 1
+    """
     def _ker(v1, v2):
         v1, v2 = np.atleast_2d(v1, v2)
         res = np.squeeze(v1 @ v2.T + 1)
@@ -11,6 +25,9 @@ def get_linear_kernel():
 
 
 def get_polynomial_kernel(p):
+    """Implements:
+        (<x.T, y> + 1)^p
+    """
     def _ker(v1, v2):
         v1, v2 = np.atleast_2d(v1, v2)
         res = np.squeeze(np.power((v1 @ v2.T + 1), p))
@@ -20,6 +37,9 @@ def get_polynomial_kernel(p):
 
 
 def get_radial_basis_function_kernel(sigma):
+    """Implements:
+        exp(- ||x - y||^2 / sigma^2)
+    """
     def _ker(v1, v2):
         v1, v2 = np.atleast_2d(v1, v2)
         aux1 = np.linalg.norm(v1 - v2, axis=1) ** 2
@@ -32,6 +52,9 @@ def get_radial_basis_function_kernel(sigma):
 
 
 def get_sigmoid_kernel(k, delta):
+    """Implements:
+        tanh(<k * x.T, y> - delta)
+    """
     def _ker(v1, v2):
         v1, v2 = np.atleast_2d(v1, v2)
         res = np.squeeze(np.tanh(k * v1 @ v2.T - delta))
