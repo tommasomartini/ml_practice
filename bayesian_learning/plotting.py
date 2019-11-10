bddd3d6e6163e7867a180e9fab19d7585aadf29f
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2019-11-10 10:42:46 +0100

    Function to plot boundaries

--- bayesian_learning/plotting.py
+++ bayesian_learning/plotting.py
@@ -4,6 +4,45 @@ from matplotlib.patches import Ellipse
 
 
 _NUM_STD_FOR_GAUSSIAN_PLOT = 2
+_DEFAULT_GRID_SIZE = (100, 100)
+
+
+def plot_boudaries(ax, classifier, xlims=None, ylims=None, grid_size=None):
+    xlims = xlims or ax.get_xlim()
+    try:
+        min_x, max_x = xlims
+    except:
+        min_x, max_x = -xlims, xlims
+
+    ylims = ylims or ax.get_ylim()
+    try:
+        min_y, max_y = ylims
+    except:
+        min_y, max_y = -ylims, ylims
+
+    grid_size = grid_size or _DEFAULT_GRID_SIZE
+    try:
+        grid_size_x, grid_size_y = grid_size
+    except:
+        grid_size_x, grid_size_y = grid_size, grid_size
+
+    xrange = np.linspace(min_x, max_x, grid_size_x)
+    yrange = np.linspace(min_y, max_y, grid_size_y)
+    xx, yy = np.meshgrid(xrange, yrange, indexing='xy')
+    zz = classifier.classify(np.c_[xx.ravel(), yy.ravel()])
+
+    cmap = cm.get_cmap('tab10')
+    zz_colors = np.array([
+        cmap((class_idx % 10) / 10)
+        for class_idx in zz
+    ])
+    color_grid = zz_colors.reshape((grid_size_y, grid_size_x, -1))
+
+    ax.imshow(color_grid,
+              origin='lower',
+              extent=[min_x, max_x, min_y, max_y],
+              alpha=0.5,
+              aspect='auto')
 
 
 def plot_gaussian_distribution_ellipse(ax, mu, sigma, **kwargs):
