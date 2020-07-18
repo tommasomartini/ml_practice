134bcf493bc4122fe2d40a44c6a4dcc551ab716f
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-07-18 20:14:08 +0200

    Add more activation functions

--- artificial_neural_networks/activations.py
+++ artificial_neural_networks/activations.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 
+class Linear:
+
+    @staticmethod
+    def fw(x):
+        return x
+
+    @staticmethod
+    def grad(x):
+        return np.ones(x.shape)
+
+
 class ReLU:
 
     @staticmethod
@@ -12,12 +23,38 @@ class ReLU:
         return (x >= 0).astype(float)
 
 
+class LeakyReLU:
+    p = 0.01
+
+    @staticmethod
+    def fw(x):
+        return np.zeros(x.shape) + x * (x >= 0) + LeakyReLU.p * x * (x < 0)
+
+    @staticmethod
+    def grad(x):
+        return np.zeros(x.shape) + 1 * (x >= 0) + LeakyReLU.p * (x < 0)
+
+
+class Tanh:
+
+    @staticmethod
+    def fw(x):
+        return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+    @staticmethod
+    def grad(x):
+        return 1 - Tanh.fw(x) ** 2
+
+
 class Logistic:
 
     @staticmethod
     def fw(x):
-        return np.exp(x) / (1 + np.exp(x))
+        return 1 / (1 + np.exp(-x))
 
     @staticmethod
     def grad(x):
-        return Logistic.fw(x) * Logistic.fw(-x)
+        return Logistic.fw(x) * (1 - Logistic.fw(x))
+
+
+Sigmoid = Logistic
